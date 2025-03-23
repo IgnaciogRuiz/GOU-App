92979301b2a5787747feb1af29ca9f31f187ce83
IgnaciogRuiz <ignacioguillermoruiz@gmail.com>
2025-03-22 22:25:41 -0300

    funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>
-        Open up App.js to start working on your app! me cago en native
+        negro
       </Text>
       <StatusBar style="auto" />
     </View>
